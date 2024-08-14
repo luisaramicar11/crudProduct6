f7eda62ce2218260449642f7164f3e53710905e8
luisaramicar11 <luisaf.ramirez.cardona11@gmail.com>
2024-08-14 08:04:05 -0500

    adding help

---
+++ src/app/components/pastel.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Link from 'next/link';
+
+const Header = () => {
+
+  return (
+    <header>
+      <h2>titulo</h2>
+      <nav>
+        <Link href="/about">About</Link>
+        <br />
+        <Link href="/contact">Contact</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
